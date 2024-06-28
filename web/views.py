a4094450d83742dd51dd084637823c50c7c1563d
Peter <mendez.pedro94@gmail.com>
2024-06-27 22:25:34 -0400

    first views

--- web/views.py
+++ web/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def inicio(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def welcome(request):
+    return render(request, 'welcome.html')
+
+
+def login(request):
+    return redirect('account/login')
