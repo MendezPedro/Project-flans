3176cbd59f99f3730359ea87fdee797be8f9beb1
Peter <mendez.pedro94@gmail.com>
2024-06-30 00:26:23 -0400

    add views and template flan

--- web/views.py
+++ web/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect
+from .models import Flan
 
 # Create your views here.
 def inicio(request):
-    return render(request, 'index.html')
+    flanes_publicos = Flan.objects.filter(is_private=False)
+    return render(request, 'index.html', context = {'flanes': flanes_publicos})
 
 def about(request):
     return render(request, 'about.html')
 
 def welcome(request):
-    return render(request, 'welcome.html')
+    flanes_privados = Flan.objects.filter(is_private=True)
+    return render(request, 'welcome.html', context = {'flanes': flanes_privados})
 
 
 def login(request):
