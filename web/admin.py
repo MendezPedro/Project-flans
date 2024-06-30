9e397e2a44f4095ed0efebc81442428c69c6b71b
Peter <mendez.pedro94@gmail.com>
2024-06-30 00:51:04 -0400

    add model ContactForm

--- web/admin.py
+++ web/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from web.models import Flan
+from web.models import Flan, ContactForm
 
 # Register your models here.
 
 admin.site.register(Flan)
+admin.site.register(ContactForm)
