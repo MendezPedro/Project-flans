f40f049161208205e3e613ce2a42011b16b5261d
Peter <mendez.pedro94@gmail.com>
2024-06-30 00:25:40 -0400

    add model flan

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from web.models import Flan
 
 # Register your models here.
+
+admin.site.register(Flan)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Flan(models.Model):
+    flan_uuid = models.UUIDField()
+    name = models.CharField(max_length = 64)
+    description = models.TextField()
+    image_url = models.URLField()
+    slug = models.SlugField()
+    is_private = models.BooleanField()
+    #precio = models.IntegerField(blank=True, null=True)
+
+    def __str__(self)-> str:
+        return self.name
