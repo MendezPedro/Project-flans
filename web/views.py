b81d4b4fa81cd00dda4c75e974946b3dda00343c
Peter <mendez.pedro94@gmail.com>
2024-07-02 20:55:54 -0400

    add view review

--- web/views.py
+++ web/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render, redirect
-from .models import Flan, ContactForm
+from .models import Flan, ContactForm, Review
 from django.http import HttpResponseRedirect
-from .forms import ContactFormForm, ContactFormModelForm
+from .forms import ContactFormForm, ContactFormModelForm, ReviewForm
 from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def inicio(request):
     flanes_publicos = Flan.objects.filter(is_private=False)
     return render(request, 'index.html', context = {'flanes': flanes_publicos})
 
+
 def about(request):
     return render(request, 'about.html')
 
 
+
 def login(request):
     return redirect('accounts/login')
+
 
 def contacto(request):
 
@@ -35,6 +39,30 @@ def contacto(request):
 
 def exito(request):
     return render(request, 'exito.html')
+
+
+def review_view(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            # Asigna el nombre de usuario de la sesión
+            if request.user.is_authenticated:
+                review.user_name = request.user.username
+            else:
+                review.user_name = 'Anónimo' 
+            review.save()
+            return redirect('review_success')
+    else:
+        form = ReviewForm()
+
+    return render(request, 'review.html', {'form': form})
+
+
+def review_success_view(request):
+    return render(request, 'review_success.html')
+
+
 
 @login_required
 def welcome(request):
