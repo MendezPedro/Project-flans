45136f6a3918e9bad4dbcea603ce74707961aaf2
Peter <mendez.pedro94@gmail.com>
2024-07-02 20:06:56 -0400

    review add admin

--- web/admin.py
+++ web/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from web.models import Flan, ContactForm
+from web.models import Flan, ContactForm, Review
 
 # Register your models here.
 
 admin.site.register(Flan)
 admin.site.register(ContactForm)
+admin.site.register(Review)
